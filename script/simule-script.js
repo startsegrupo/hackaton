0befac9ea5ab740233927d44b55983e0a78c49d5
startsegrupo <110001627+startsegrupo@users.noreply.github.com>
2022-07-27 21:22:16 -0300

    Add files via upload

--- script/simule-script.js
+++ script/simule-script.js
@@ -1,2 +1,2 @@
-const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
-const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle');
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl));
