b3182ec7c38c5dc80440ac481742a21c061cec08
startsegrupo <110001627+startsegrupo@users.noreply.github.com>
2022-07-28 22:37:45 -0300

    Add files via upload

---
+++ script/dashboard.js
@@ -0,0 +1,12 @@
+(function($) {
+    "use strict";   
+    $(document).on('scroll', function() {
+      var scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+  })(jQuery);
+  
--- script/simule-script.js
+++ script/simule-script.js
@@ -1,2 +1,7 @@
 const dropdownElementList = document.querySelectorAll('.dropdown-toggle');
 const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl));
+
+$('#datepicker').datepicker({
+    inline: true
+  });
+
